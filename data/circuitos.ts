f913a0d7620b3b99f40b8d7b6c3d5ec189c251e1
MrRay46 <franciscodelaraya@gmail.com>
2026-05-24 14:32:00 +0200

    Add season predictions >> >> >> >> >> >> >> >> >> >> >> >> >> >> >> types

--- data/circuitos.ts
+++ data/circuitos.ts
@@ -1,4 +1,52 @@
 export const circuitos = [
+     {
+    nombre: "Chang International Circuit",
+    pais: "Thailandia",
+    fechaInicio: "2026-02-27",
+    fechaFin: "2026-03-01",
+    imagen: "/circuitos/thailandia.png",
+  },
+
+   {
+    nombre: "Ayrton Senna",
+    pais: "Brasil",
+    fechaInicio: "2026-03-20",
+    fechaFin: "2026-03-22",
+    imagen: "/circuitos/brasil.png",
+  },
+
+  {
+    nombre: "Circuit Of The Americas",
+    pais: "USA",
+    fechaInicio: "2026-03-27",
+    fechaFin: "2026-03-29",
+    imagen: "/circuitos/usa.png",
+  },
+
+  {
+    nombre: "Circuito de Jerez - Ángel Nieto",
+    pais: "España",
+    fechaInicio: "2026-04-24",
+    fechaFin: "2026-04-26",
+    imagen: "/circuitos/jerez.png",
+  },
+
+  {
+    nombre: "Le Mans",
+    pais: "Francia",
+    fechaInicio: "2026-05-08",
+    fechaFin: "2026-05-10",
+    imagen: "/circuitos/le mans.png",
+  },
+
+  {
+    nombre: "Circuit de Barcelona-Catalunya",
+    pais: "España",
+    fechaInicio: "2026-05-15",
+    fechaFin: "2026-05-17",
+    imagen: "/circuitos/cataluña.png",
+  },
+
   {
     nombre: "Mugello",
     pais: "Italia",
@@ -8,7 +56,7 @@ export const circuitos = [
   },
 
   {
-    nombre: "Gran premio de Hungria",
+    nombre: "Balaton Park",
     pais: "Hungria",
     fechaInicio: "2026-06-05",
     fechaFin: "2026-06-07",
@@ -21,5 +69,117 @@ export const circuitos = [
     fechaInicio: "2026-06-19",
     fechaFin: "2026-06-21",
     imagen: "/circuitos/republica checa.png",
+  },
+
+  {
+    nombre: "TT Circuit Assen",
+    pais: "Holanda",
+    fechaInicio: "2026-06-26",
+    fechaFin: "2026-06-28",
+    imagen: "/circuitos/assen.png",
+  },
+
+  {
+    nombre: "Sachsenring",
+    pais: "Alemania",
+    fechaInicio: "2026-06-26",
+    fechaFin: "2026-06-28",
+    imagen: "/circuitos/Sachsenring.png",
+  },
+
+  {
+    nombre: "Sachsenring",
+    pais: "Alemania",
+    fechaInicio: "2026-07-10",
+    fechaFin: "2026-07-12",
+    imagen: "/circuitos/Sachsenring.png",
+  },
+
+  {
+    nombre: "Silverstone Circuit",
+    pais: "Gran Bretaña",
+    fechaInicio: "2026-08-07",
+    fechaFin: "2026-08-09",
+    imagen: "/circuitos/Silverstone.png",
+  },
+
+  {
+    nombre: "MotorLand Aragón",
+    pais: "España",
+    fechaInicio: "2026-08-28",
+    fechaFin: "2026-08-30",
+    imagen: "/circuitos/Aragon.png",
+  },
+
+   {
+    nombre: "Misano World Circuit Marco Simoncelli",
+    pais: "Misano",
+    fechaInicio: "2026-09-11",
+    fechaFin: "2026-09-13",
+    imagen: "/circuitos/misano.png",
+  },
+
+  {
+    nombre: "Red Bull Ring - Spielberg",
+    pais: "Austria",
+    fechaInicio: "2026-09-18",
+    fechaFin: "2026-09-20",
+    imagen: "/circuitos/austria.png",
+  },
+
+  {
+    nombre: "Mobility Resort Motegi",
+    pais: "Japon",
+    fechaInicio: "2026-10-01",
+    fechaFin: "2026-10-04",
+    imagen: "/circuitos/motegi.png",
+  },
+
+  {
+    nombre: "Pertamina Mandalika",
+    pais: "Indonesia",
+    fechaInicio: "2026-10-09",
+    fechaFin: "2026-10-11",
+    imagen: "/circuitos/indonesia.png",
+  },
+
+   {
+    nombre: "Phillip Island",
+    pais: "Australia",
+    fechaInicio: "2026-10-22",
+    fechaFin: "2026-10-25",
+    imagen: "/circuitos/phillip island.png",
+  },
+
+  {
+    nombre: "Sepang",
+    pais: "Malasia",
+    fechaInicio: "2026-10-30",
+    fechaFin: "2026-11-01",
+    imagen: "/circuitos/sepang.png",
+  },
+
+   {
+    nombre: "Lusail",
+    pais: "Qatar",
+    fechaInicio: "2026-11-06",
+    fechaFin: "2026-11-08",
+    imagen: "/circuitos/lusail.png",
+  },
+
+  {
+    nombre: "Autódromo Internacional do Algarve",
+    pais: "Portugal",
+    fechaInicio: "2026-11-20",
+    fechaFin: "2026-11-22",
+    imagen: "/circuitos/portugal.png",
+  },
+
+   {
+    nombre: "Circuit Ricardo Tormo",
+    pais: "España",
+    fechaInicio: "2026-11-27",
+    fechaFin: "2026-11-29",
+    imagen: "/circuitos/ricardo tormo cheste.png",
   },
 ];
