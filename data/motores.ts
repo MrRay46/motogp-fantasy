0b436ee776165962685dfac069358f31bf957cd3
MrRay46 <franciscodelaraya@gmail.com>
2026-05-31 18:39:41 +0200

    Migracion a Supabase completada

--- data/motores.ts
+++ data/motores.ts
@@ -2,31 +2,31 @@ export const motores = [
   {
     nombre: "Ducati",
     precio: 6,
-    puntos: 48,
+    puntos: 56,
     logo: "/marcas/ducati.png",
   },
   {
     nombre: "Aprilia",
     precio: 4.1,
-    puntos: 54,
+    puntos: 64,
     logo: "/marcas/aprilia.png",
   },
   {
     nombre: "KTM",
     precio: 3.4,
-    puntos: 37,
+    puntos: 43,
     logo: "/marcas/ktm.png",
   },
   {
     nombre: "Honda",
     precio: 2.3,
-    puntos: 26,
+    puntos: 30,
     logo: "/marcas/honda.png",
   },
   {
     nombre: "Yamaha",
     precio: 1.7,
-    puntos: 16, 
+    puntos: 12, 
     logo: "/marcas/yamaha.png",
   },
 ];
