50ca2740323248fa453fbb03d6394e5826d118f2
MrRay46 <franciscodelaraya@gmail.com>
2026-06-13 17:29:06 +0200

    login real

---
+++ app/registro/page.tsx
@@ -0,0 +1,15 @@
+export default function RegistroPage() {
+
+  return (
+
+    <main className="min-h-screen bg-black text-white flex items-center justify-center">
+
+      <h1 className="text-5xl font-bold">
+        Registro de Usuario
+      </h1>
+
+    </main>
+
+  );
+
+}
