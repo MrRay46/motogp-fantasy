ec14187b824f5e840c21b638c1a9aee3e816123e
MrRay46 <franciscodelaraya@gmail.com>
2026-07-25 19:27:05 +0200

    equipo

--- components/equipo/PredictionsCard.tsx
+++ components/equipo/PredictionsCard.tsx
@@ -30,7 +30,7 @@ export default function PredictionsCard({
         🎯 Predicciones
       </h2>
 
-      <div className="space-y-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
 
         {/* Campeón */}
 
